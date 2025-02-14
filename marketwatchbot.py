5bb2f7a90df8771f68312806272058f51f7d88d2
Andrew Silkwood <asilkwoo@asu.edu>
2025-02-13 21:01:53 -0700

    add cog support

--- marketwatchbot.py
+++ marketwatchbot.py
@@ -1,40 +1,94 @@
 from dotenv import load_dotenv
 import os
+import re
+
 import discord
 import logging
 from discord.ext import commands
 
 # Load .env
 load_dotenv()
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-CMD_PREFIX = os.getenv('CMD_PREFIX')
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CMD_PREFIX = os.getenv("CMD_PREFIX")
 
 # Global Variables
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix=CMD_PREFIX, intents=intents)
 
+
 def main():
     # load in .env variables
-    BOT_TOKEN = os.getenv('BOT_TOKEN')
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
 
     # initialize bot
-    handler = logging.FileHandler(filename='marketwatch.log', encoding='utf-8', mode='w')
+    handler = logging.FileHandler(
+        filename="marketwatch.log", encoding="utf-8", mode="w"
+    )
     bot.run(BOT_TOKEN, log_handler=handler, log_level=logging.DEBUG)
 
 
-# General Bot Events
+# Bot Events
+
 
 # on bot initialized
 @bot.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
+    print(f"We have logged in as {bot.user}")
 
-@bot.command()
+    # load all files in the cogs folder
+    print("Loading cogs")
+    for f in os.listdir("./cogs/"):
+        if re.match(r"\w+\.py", f):
+            try:
+                await bot.load_extension(f"cogs.{f[:-3]}")
+                print(f"Loaded {f[:-3]}")
+            except:
+                print(f"Failed to load {f[:-3]}")
+
+    print("Cogs have been loaded")
+
+
+# Bot Control Commands
+
+
+# shutdown bot gracefully
+@bot.command(aliases=["sd", "quit", "close"])
 @commands.is_owner()
 async def shutdown(ctx):
+    await ctx.send("Shutting down...")
     await bot.close()
 
 
-if __name__ == '__main__':
+# loads a single file in the cogs folder
+@bot.command()
+@commands.is_owner()
+async def load(ctx, arg):
+    try:
+        await bot.load_extension(f"cogs.{arg}")
+        print(f"Reloaded {arg}")
+        await ctx.send(f"Loaded {arg}")
+    except:
+        print(f"Failed to reload {arg}")
+        await ctx.send(f"Failed to load '{arg}'")
+
+
+# reloads all files in the cogs folder
+@bot.command()
+@commands.is_owner()
+async def reload(ctx):
+    print("Reloading cogs")
+    for ext in list(bot.extensions.keys()):
+        try:
+            await bot.reload_extension(ext)
+            print(f"Reloaded {ext[5:]}")
+        except:
+            print(f"Failed to reload {ext[5:]}")
+            await ctx.send(f"Failed to load '{ext[5:]}'")
+
+    print("Cogs have been reloaded")
+    await ctx.send(f"Reload complete")
+
+
+if __name__ == "__main__":
     main()
